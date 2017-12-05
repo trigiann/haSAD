dc8ea071eef9debdb2390d1e70a8a277b07f5588
trigiann <34131995+trigiann@users.noreply.github.com>
2017-12-05 17:29:24 +0100

    Add files via upload

---
+++ client.go
@@ -0,0 +1,43 @@
+package main
+ 
+import (
+        "fmt"
+        "log"
+        "net"
+        "bufio"
+        "os"
+)
+ 
+func main() {
+        fmt.Print("Choose a nickname: ")
+        r := bufio.NewReader(os.Stdin)
+        nick,_,_ := r.ReadLine()
+        conn, err := net.Dial("tcp", ":7000")
+        _,_ = conn.Write(nik)
+
+        if err != nil {
+                log.Fatalln(err)
+        }
+        for {
+                data := read()
+                _, err = conn.Write(nick, data)
+                if err != nil {
+                        log.Fatalln(err)
+                }
+                buffer := make([]byte, 1400)
+                dataSize, err := conn.Read(buffer)
+                if err != nil {
+                        fmt.Println("connection closed")
+                        return
+                }
+ 
+                info := buffer[:dataSize]
+                fmt.Println("received message: ", string(info))
+        }
+}
+
+func read () []byte {
+        reader := bufio.NewReader(os.Stdin)
+        data,_,_ := reader.ReadLine()
+        return data
+} 
