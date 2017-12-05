faaa11e44da3a2a2938e5b62d042c5f48ea17b9f
trigiann <34131995+trigiann@users.noreply.github.com>
2017-12-05 17:30:19 +0100

    Add files via upload

---
+++ server-tcp.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+var (clients = make (map[string]net.Conn))
+
+func main() {
+	server, err := net.Listen("tcp", ":7000")
+	fmt.Println("server listening on 7000")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer server.Close()
+
+	for {
+		conn, err := server.Accept()
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+		go listenConnection(conn)
+		fmt.Println("new connection: ")
+	}
+		
+}
+
+
+func listenConnection(conn net.Conn) {
+	buffer := make([]byte, 1400)
+    dataSize, err := conn.Read(buffer)
+    if err != nil {
+            fmt.Println("connection closed")
+            return
+    }
+    nick := buffer[:dataSize]
+    clients[string(nick)] = conn
+
+	for {
+		//buffer := make([]byte, 1400)
+        dataSize, err := conn.Read(buffer)
+        if err != nil {
+                fmt.Println("connection closed")
+                return
+        }
+       
+        data := buffer[:dataSize]
+        fmt.Println("received message: ", string(data))
+       
+        _, err = conn.Write(data)
+        if err != nil {
+                log.Fatalln(err)
+        }
+        fmt.Println("Message sent: ", string(data))
+	}
+}
