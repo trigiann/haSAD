4cee29f9c6341f1be52a37627388ee71ac09526a
trigiann <34131995+trigiann@users.noreply.github.com>
2017-12-08 11:38:59 +0100

    Add files via upload

--- client.go
+++ client.go
@@ -4,27 +4,28 @@ import (
         "fmt"
         "log"
         "net"
+        "bufio"
+        "os"
 )
  
 func main() {
+        // fmt.Print("Choose a nickname: ")
+        // r := bufio.NewReader(os.Stdin)
+        // nick,_,_ := r.ReadLine()
+        // conn, err := net.Dial("tcp", ":7000")
+        // _,_ = conn.Write(nick)
+
         conn, err := net.Dial("tcp", ":7000")
+
         if err != nil {
                 log.Fatalln(err)
         }
- 
-        _, err = conn.Write([]byte("Hello Server!"))
-        if err != nil {
-                log.Fatalln(err)
-        }
-        fmt.Println("Hem saludat al server")
- 
-        _, err = conn.Write([]byte("How are you?"))
-        if err != nil {
-                log.Fatalln(err)
-        }
-        fmt.Println("Hem preguntat com esta el server")
- 
         for {
+                data := read()
+                _, err = conn.Write(data)
+                if err != nil {
+                        log.Fatalln(err)
+                }
                 buffer := make([]byte, 1400)
                 dataSize, err := conn.Read(buffer)
                 if err != nil {
@@ -32,7 +33,13 @@ func main() {
                         return
                 }
  
-                data := buffer[:dataSize]
-                fmt.Println("received message: ", string(data))
+                info := buffer[:dataSize]
+                fmt.Println("received message: ", string(info))
         }
 }
+
+func read () []byte {
+        reader := bufio.NewReader(os.Stdin)
+        data,_,_ := reader.ReadLine()
+        return data
+} 
--- server-tcp.go
+++ server-tcp.go
@@ -6,6 +6,8 @@ import (
 	"net"
 )
 
+//var (clients = make (map[net.Conn]string))
+
 func main() {
 	server, err := net.Listen("tcp", ":7000")
 	fmt.Println("server listening on 7000")
@@ -16,31 +18,73 @@ func main() {
 
 	for {
 		conn, err := server.Accept()
+
 		if err != nil {
 			log.Fatalln(err)
 		}
-		fmt.Println("new connection")
-
 		go listenConnection(conn)
+		fmt.Println("new connection: ")
 	}
+		
 }
+
 
 func listenConnection(conn net.Conn) {
+	// buffer := make([]byte, 1400)
+ //    dataSize, err := conn.Read(buffer)
+ //    if err != nil {
+ //            fmt.Println("connection closed")
+ //            return
+ //    }
+ //    nick := buffer[:dataSize]
+    
+
 	for {
 		buffer := make([]byte, 1400)
-                dataSize, err := conn.Read(buffer)
-                if err != nil {
-                        fmt.Println("connection closed")
-                        return
-                }
-               
-                data := buffer[:dataSize]
-                fmt.Println("received message: ", string(data))
-               
-                _, err = conn.Write(data)
-                if err != nil {
-                        log.Fatalln(err)
-                }
-                fmt.Println("Message sent: ", string(data))
+        dataSize, err := conn.Read(buffer)
+        if err != nil {
+                fmt.Println("connection closed")
+                return
+        }
+       
+        data := buffer[:dataSize]
+        fmt.Println("received message: ", string(data))
+       
+        _, err = conn.Write(data)
+        if err != nil {
+                log.Fatalln(err)
+        }
+        fmt.Println("Message sent: ", string(data))
 	}
 }
+
+
+/*func listenConnection(conn net.Conn) {
+	buffer := make([]byte, 1400)
+ 	dataSize,_ := conn.Read(buffer)
+    nick := buffer[:dataSize]
+    
+	//Register NEW client
+	clients[conn] = string(nick)
+
+	for clients := range clients {
+		dataSize, err := conn.Read(buffer)
+        if err != nil {
+                fmt.Println("connection closed")
+                return
+        }
+       
+        data := buffer[:dataSize]
+        fmt.Println("received message: ", string(data))
+       
+        _, err = clients.Write(data)
+        if err != nil {
+                log.Fatalln(err)
+        }
+        fmt.Println("Message sent: ", string(data))
+
+
+	}
+
+
+}*/
